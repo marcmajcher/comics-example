53207405f5da84c027cc4619001d8ff93ec1f4ac
Marc Majcher <majcher@gmail.com>
2018-07-02 13:39:13 -0500

    first get route

--- models/comics.js
+++ models/comics.js
@@ -3,19 +3,19 @@
 
 const comicsList = [
     {
-        name: 'The Amazing Spider-Man',
+        title: 'The Amazing Spider-Man',
         issue: 252,
         author: 'Steve Ditko',
         description: 'Spidey comes back from Secret Wars with his new black suit!'
     },
     {
-        name: 'Invincible Iron Man',
+        title: 'Invincible Iron Man',
         issue: 55,
         author: 'Stan Lee',
         description: 'First appearance of Thanos'
     },
     {
-        name: 'Groo The Wanderer',
+        title: 'Groo The Wanderer',
         issue: 1,
         author: 'Sergio Arrigones',
         description: 'First issue'
--- routes/comics.js
+++ routes/comics.js
@@ -4,13 +4,12 @@
 const express = require('express');
 const router = express.Router();
 const Comic = require('../models/comics');
-console.log(Comic);
 
 router.get('/', (req, res) => {
-  console.log(Comic.list)
-   Comic.list().then((data) => {
-    console.log(data);
-    res.send(data);
+  Comic.list().then((data) => {
+    res.render('comics', {
+      data
+    });
   });
 });
 
