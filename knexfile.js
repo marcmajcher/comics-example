21c20808ebdfe8080be9295dc1807cf9cf6dc62f
Marc Majcher <majcher@gmail.com>
2018-07-02 14:02:23 -0500

    prod

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'wherever the hell our database lives'
+    connection: 'postgres://aqwmgbbrkbkazo:75035cf835e85884aa9260eb02f33bbbac8be2ef009f7055c000cd9194c3d0a9@ec2-54-163-237-249.compute-1.amazonaws.com:5432/d23ujh8rck5c37'
   }
 };
--- routes/comics.js
+++ routes/comics.js
@@ -1,16 +1,22 @@
 'use strict';
+
 /* eslint-env node */
+/* eslint-disable no-use-before-define */
 
 const express = require('express');
 const router = express.Router();
 const Comic = require('../models/comics');
 
-router.get('/', (req, res) => {
+router.get('/', listComics);
+
+/* router functions */
+
+function listComics(req, res) {
   Comic.list().then((data) => {
     res.render('comics', {
       data
     });
   });
-});
+}
 
 module.exports = router;
